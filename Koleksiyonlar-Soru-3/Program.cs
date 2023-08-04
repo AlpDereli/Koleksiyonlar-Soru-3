b79d2a88a8ad72fe9311ce8c1a56624863e7e8fa
AlpDereli <alpdereli26@gmail.com>
2023-08-04 20:08:54 +0300

    Add project files.

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<char> sesliler = new List<char>();
+Console.WriteLine("Bir cumle giriniz: ");
+string s = Console.ReadLine();
+string vowels = "AEIOUaeiou";
+foreach (char c in s)
+{
+    if (vowels.Contains(c))
+    {
+        sesliler.Add(c);
+    }
+}
+foreach(char c in sesliler)
+{ Console.WriteLine(c); }
+
